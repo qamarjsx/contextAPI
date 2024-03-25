bb841575d54d32fa5be94758b965b77d56fc9cbd
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-03-25 21:48:38 +0500

    initial files and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-950 h-screen w-full grid place-items-center">
+      <h1 className="text-white ">CONTEXT API</h1>      
+      </div>
+    </>
+  )
+}
+
+export default App
